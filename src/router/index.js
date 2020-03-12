c451500a739b22581dc901d70a1f069776470d3a
fly <1490493387@qq.com>
2020-03-12 23:56:17 +0800

    框架搭建

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,36 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const router =new Router({
-    scrollBehavior: () => ({ y: 0 }),
-    routes:[{
+const router = new Router({
+    scrollBehavior: () => ({y: 0}),
+    routes: [{
         path: '/',
-        component : ()=>import('@/views/dashboard')
+        redirect: '/dashboard',
+        meta: {
+            //是否缓存数据
+            keepAlive: true
+        }
+    }, {
+        // 主框架页面
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/dashboard'),
+        children: [{
+            path: '/dashboard',
+            redirect: '/dashboard/home',
+            meta: {
+                keepAlive: true
+            }
+        }, {
+            path: 'home',
+            name: 'home',
+            component: () => import("@/views/home"),
+            meta: {
+                keepAlive: true
+            }
+        }
+
+        ]
     }]
 
 })
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,13 +1,19 @@
 <!--项目起始页-->
 <template>
     <div id="dashboard">
+
+        <!-- 按需缓存需要缓存的页面，在router中设置router的元信息meta的keepAlive -->
+        <keep-alive>
+            <router-view v-if="$route.meta.keepAlive"></router-view>
+        </keep-alive>
+        <router-view v-if="!$route.meta.keepAlive"></router-view>
+
         <van-tabbar v-model="active"
                     :safe-area-inset-bottom=true
-                    active-color="#75a342"
-        >
-            <van-tabbar-item  v-for="(item,index) in tabbars"
-                :key="index" :id="item.name=='cart'?'shop-cart':''"
-                :info="item.name=='cart' ? 5: ''"
+                    active-color="#75a342">
+            <van-tabbar-item v-for="(item,index) in tabbars"
+                             :key="index" :id="item.name=='cart'?'shop-cart':''"
+                             :info="item.name=='cart' ? 5: ''"
             >
                 <span :class="currentIndex==index? active :''">{{item.title}}</span>
                 <img
@@ -23,6 +29,21 @@
 <script>
     export default {
         name: "dashboard",
+        mounted () {
+        },
+        created () {
+            //通过路由跳转绑定Tabbar的选中
+            this.tabbarSelected(this.$route.name);
+        },
+        watch: {
+            // 监听路由变化,保证路由跳转Tabbar选中正常
+            $route: {
+                handler(val, oldval) {
+                    this.tabbarSelected(val.name);
+                }
+            },
+            deep: true
+        },
         data() {
             return {
                 currentIndex: 0,
@@ -55,6 +76,18 @@
                     }
 
                 ]
+            }
+        },
+
+        methods: {
+            tabbarSelected(item) {
+                const mapType = {
+                    home: 0,
+                    category: 1,
+                    cart: 2,
+                    mine: 3
+                }
+                this.active = mapType[item]
             }
         }
     }
