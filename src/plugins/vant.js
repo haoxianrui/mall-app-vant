62c5bf68163ed58e33fa1793ec84b9eadc00816d
XCZTH <1490493387@qq.com>
2020-03-17 09:28:59 +0800

    添加秒杀商品

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -3,43 +3,62 @@
 import Vue from 'vue'
 
 // Tabbar底部
-import { Tabbar, TabbarItem } from 'vant'
+import {Tabbar, TabbarItem} from 'vant'
+
 Vue.use(Tabbar).use(TabbarItem)
 
 // loading组件
-import { Loading } from 'vant'
+import {Loading} from 'vant'
+
 Vue.use(Loading)
 
 // 轮播组件
-import {Swipe,SwipeItem} from "vant"
+import {Swipe, SwipeItem} from "vant"
+
 Vue.use(Swipe).use(SwipeItem)
 
 
 // 懒加载
 import {Lazyload} from "vant"
+
 Vue.use(Lazyload)
 
 // 栅栏宫格
 import {Grid, GridItem} from 'vant'
+
 Vue.use(Grid).use(GridItem)
 
 // icon图标
-import { Icon } from "vant"
+import {Icon} from "vant"
+
 Vue.use(Icon)
 
 
 // panel面板
-import { Panel } from 'vant'
+import {Panel} from 'vant'
+
 Vue.use(Panel)
 
 // 倒计时
-import { CountDown } from 'vant'
+import {CountDown} from 'vant'
+
 Vue.use(CountDown)
 
 // 分割线
-import { Divider } from 'vant'
+import {Divider} from 'vant'
+
 Vue.use(Divider)
 
 // image
-import { Image } from "vant"
+import {Image} from "vant"
 Vue.use(Image)
+
+
+// 商品卡片
+import {Card} from 'vant';
+
+Vue.use(Card);
+
+// 按钮
+import {Button} from 'vant'
+Vue.use(Button)
--- src/views/home/components/seckill/goods.vue
+++ src/views/home/components/seckill/goods.vue
@@ -1,15 +1,33 @@
 <template>
-    <van-grid :border="true" :column-num="3" >
-        <van-grid-item>
-            <van-image src="https://img.yzcdn.cn/vant/apple-1.jpg" />
-        </van-grid-item>
-        <van-grid-item>
-            <van-image src="https://img.yzcdn.cn/vant/apple-2.jpg" />
-        </van-grid-item>
-        <van-grid-item>
-            <van-image src="https://img.yzcdn.cn/vant/apple-3.jpg" />
-        </van-grid-item>
-    </van-grid>
+   <div class="main">
+       <van-grid :border="true" :column-num="1" >
+           <van-grid-item>
+               <van-card
+                       tag="秒杀"
+                       price="2.00"
+                       desc="描述信息描述信息描述信息描述信息"
+                       title="商品标题描述信息描述信息描述信息"
+                       thumb="https://img.yzcdn.cn/vant/apple-1.jpg"
+                       origin-price="10.00"
+               >
+                   <div slot="footer">
+                       <van-button size="mini">按钮</van-button>
+                       <van-button size="mini">按钮</van-button>
+                   </div>
+
+               </van-card>
+           </van-grid-item>
+           <van-grid-item>
+               <van-image src="https://img.yzcdn.cn/vant/apple-2.jpg" />
+           </van-grid-item>
+           <van-grid-item>
+               <van-image src="https://img.yzcdn.cn/vant/apple-3.jpg" />
+           </van-grid-item>
+           <van-grid-item>
+               <van-image src="https://img.yzcdn.cn/vant/apple-3.jpg" />
+           </van-grid-item>
+       </van-grid>
+   </div>
 </template>
 
 <script>
@@ -18,6 +36,12 @@
     }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+    .main{
+        .van-grid-item{
+            height: 200px;
+        }
+
+    }
 
 </style>
